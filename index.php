9802837effc01a270b1e7365e67bac3f91ff991f
Ratstang <ratstang@digis.net>
2019-07-18 15:46:03 -0600

    Initial Source

---
+++ index.php
@@ -0,0 +1,75 @@
+<?php
+// Display only your coin.
+$displayCoin = 'znz';
+// Available Options: all, btc, eth, ltc, dash, send, cdzc, arrr, colx, znz.
+
+//Get Data from CryptocurrencyCheckout CryptoStreamers API
+$curl = curl_init('https://cryptocurrencycheckout.com/streamers_api?coin=' . $displayCoin);
+curl_setopt($curl, CURLOPT_FAILONERROR, true);
+curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);  
+$results = curl_exec($curl);
+$cryptoStreamers = json_decode($results);
+?>
+
+<html>
+
+  <head>
+    <!-- Bootstrap CDN for demo styling. Can use your own css -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  </head>
+
+  
+  <body>
+    <div class="container">
+
+        <!-- Bring in Twitch Script -->
+      <script src= "https://player.twitch.tv/js/embed/v1.js"></script>
+
+      <div class="row mx-auto">
+
+        <!-- Foreach to display each stream individually -->
+        <?php foreach ($cryptoStreamers->data as $value) { ?>
+
+          <div class="mx-auto col-xl-4 col-lg-6 col-sm-12 pt-3">
+
+              <!-- Box where streams will be displayed -->
+              <?php echo '<div id="twitch_player_' . $value->stream_id . '"></div>' ?>
+
+          </div>
+
+        <!-- Script Options for Each Twitch Window -->
+        <script type="text/javascript">
+          var options = {
+            width: 350,
+            height: 197,
+            <?php echo 'channel: "' . $value->stream_id . '",' ?>
+            autoplay: false
+          };   
+          <?php echo 'var player = new Twitch.Player("twitch_player_' . $value->stream_id . '", options); '?>
+          
+          player.setVolume(0.5);
+        </script>
+
+      <?php } ?>
+
+    </div>
+      
+    </div>
+
+  </body>
+
+</html>
+
+
+
+
+
+
+
+
+
+
